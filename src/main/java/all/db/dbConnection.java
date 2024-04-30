4a5817f42ecff3644e0db8575f08afe9f7f9b462
nguyenvu999 <120709343+nguyenvu999@users.noreply.github.com>
2024-05-01 03:39:10 +0700

    Add files via upload

--- src/main/java/all/db/dbConnection.java
+++ src/main/java/all/db/dbConnection.java
@@ -10,7 +10,7 @@ public class dbConnection {
             // Correct the URL format here and include SSL mode if necessary
             String url = "jdbc:postgresql://aws-0-ap-southeast-1.pooler.supabase.com:5432/" + dbname + "?sslmode=require";
             conn = DriverManager.getConnection(url, user, pass);
-            System.out.println("Connected to PostgreSQL database");
+
         } catch (Exception e) {
             System.out.println("Connection error: " + e.getMessage());
             e.printStackTrace();
